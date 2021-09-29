f09ccd98fb66e8d731e5dcd744c6c8dc9e13b7b8
Feimi Yu <yuf2@rpi.edu>
2021-09-29 18:28:10 -0400

    Setting-dependent energy plot

--- src/energy.py
+++ src/energy.py
@@ -1,7 +1,9 @@
 import sys
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import os
 import pandas as pd
 from tabulate import tabulate
 import yaml
@@ -16,23 +18,32 @@ to_newton = 1e-5  # Convert from dyne to Newton
 to_pa = 1e-1  # Convert from dyne/cm^2 to Pascal
 to_watt = 1e-7  # Convert from erg/s to Watt
 
-# Read config file
-open_glottis = [0.0, 1.0]
-output_dir = str()
-with open("./plot_settings.yaml") as plot_configs:
-    documents = yaml.full_load(plot_configs)
-    open_glottis[0] = documents["open phase"]
-    open_glottis[1] = documents["close phase"]
-    output_dir = documents["output directory"]
-    timespan = documents["time span"]
-
-    for item, doc in documents.items():
-        print(item, ":", doc)
-
+# Read the data
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
     filename = "./control_volume_analysis.csv"
+
+# Read config file
+open_glottis = [0.0, 1.0]
+output_dir = str()
+working_dir = str()
+if len(sys.argv) > 2:
+    config_filename = sys.argv[2]
+else:
+    config_filename = filename.replace(
+        "control_volume_analysis.csv", "plot_settings.yaml")
+    working_dir = os.path.dirname(filename)
+    print(working_dir)
+with open(config_filename) as plot_configs:
+    documents = yaml.full_load(plot_configs)
+    open_glottis[0] = documents["open phase"]
+    open_glottis[1] = documents["close phase"]
+    output_dir = os.path.join(working_dir, documents["output directory"])
+    timespan = documents["time span"]
+
+    for item, doc in documents.items():
+        print(item, ":", doc)
 
 # Read CV data file
 cv_data = pd.read_csv(filename, header=0)
@@ -40,9 +51,6 @@ cv_data = pd.read_csv(filename, header=0)
 # Time
 time = cv_data["Time"]
 # Time span
-# timespan = [0.1458, 0.1502]  # for power, 1.0kPa
-# timespan = [0.1766, 0.1810]  # for power, 1.0kPa
-# timespan = [0.1678, 0.1722]  # for power, 1.0kPa
 n_period = 1.0
 normalized_timespan = [0, 1]
 time_to_plot = timespan[1] - timespan[0]
@@ -54,21 +62,46 @@ def create_energy_frame(cv_data):
     # Copy energy columns
     cv_energy = cv_data[["Time", "Normalized time"]].copy()
     # Rate KE
-    # cv_energy["Rate KE"] = -cv_data["Rate KE"]
-    cv_energy["Rate KE"] = -cv_data["Rate KE direct"]
+    if ("Rate KE" in documents["energy"]):
+        cv_energy["Rate KE"] = -cv_data["Rate KE direct"]
     # Efflux
-    cv_energy["-KE efflux"] = cv_data["Inlet KE flux"] - \
-        cv_data["Outlet KE flux"]
-    cv_energy["-Convective KE"] = -cv_data["Convective KE"]
-    cv_energy["Rate dissipation"] = -cv_data["Rate dissipation"]
-    cv_energy["Rate compression work"] = cv_data["Rate compression work"]
-    cv_energy["Rate friction work"] = -cv_data["Rate friction work"]
-    cv_energy["Pressure drive"] = cv_data["Inlet pressure work"] - \
-        cv_data["Outlet pressure work"]
+    if ("-KE efflux" in documents["energy"]):
+        cv_energy["-KE efflux"] = cv_data["Inlet KE flux"] - \
+            cv_data["Outlet KE flux"]
+    if ("Rate dissipation" in documents["energy"]):
+        cv_energy["Rate dissipation"] = -cv_data["Rate dissipation"]
+    if ("Rate compression work" in documents["energy"]):
+        cv_energy["Rate compression work"] = cv_data["Rate compression work"]
+    if ("Rate friction work" in documents["energy"]):
+        cv_energy["Rate friction work"] = -cv_data["Rate friction work"]
+    if ("Pressure drive" in documents["energy"]):
+        cv_energy["Pressure drive"] = cv_data["Inlet pressure work"] - \
+            cv_data["Outlet pressure work"]
     # VF work
-    # cv_energy["Rate VF work"] = cv_data["Rate VF work"]
-    cv_energy["Rate VF work"] = -cv_data["Pressure convection"] - cv_energy["Pressure drive"]\
-        - cv_data["Rate compression work"]
+    if ("Rate VF work" in documents["energy"]):
+        cv_energy["-Convective KE"] = -cv_data["Convective KE"]
+        cv_energy["Rate VF work"] = -cv_data["Pressure convection"] - cv_energy["Pressure drive"]\
+            - cv_data["Rate compression work"]
+    # Stabilization
+    if ("Stabilization" in documents["energy"]):
+        cv_energy["Stabilization"] = cv_data["Rate stabilization"]
+    # Turbulence
+    if ("Rate turbulent momentum transfer" in documents["energy"]):
+        cv_energy["Rate turbulent momentum transfer"] = - cv_data["Rate turbulence work"]\
+            - cv_data["Rate turbulence efflux"]
+
+    def get_or_none(name: str):
+        try:
+            return cv_energy[name]
+        except KeyError:
+            return 0
+    # Residual
+    cv_energy["Residual"] = get_or_none("Pressure drive") + \
+        get_or_none("-KE efflux") + get_or_none("Rate KE") + \
+        get_or_none("Rate VF work") + get_or_none("Rate compression work") + \
+        get_or_none("Rate dissipation") + get_or_none("Rate friction work") - \
+        get_or_none("Stabilization") +\
+        get_or_none("Rate turbulent momentum transfer")
 
     # Convert the unit
     for label, content in cv_energy.items():
@@ -83,12 +116,6 @@ cv_energy = create_energy_frame(cv_data)
 for label, content in cv_energy.items():
     if label != "Time" and label != "Normalized time":
         direct_smooth(cv_energy, label, label, 100)
-# Residual
-cv_energy["Residual"] = cv_energy["Pressure drive"] + \
-    cv_energy["-KE efflux"] + cv_energy["Rate KE"] + \
-    cv_energy["Rate VF work"] + cv_energy["Rate compression work"] + \
-    cv_energy["Rate dissipation"] + cv_energy["Rate friction work"]
-
 
 # Figure properties
 height = 938/80
@@ -105,8 +132,11 @@ def apply_fig_settings(fig):
     plt.locator_params(axis='y', nbins=8)
     fig.tick_params(direction='in', length=20,
                     width=2, top=True, right=True)
-    # fig.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    fig.get_legend().remove()
+    if "legend" in documents and documents["legend"] == True:
+        print(documents["legend"])
+        fig.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    else:
+        fig.get_legend().remove()
     fig.grid()
     fig.set_xlim(normalized_timespan)
     fig.set_xlabel("t/T", fontsize=axis_label_size)
@@ -114,9 +144,11 @@ def apply_fig_settings(fig):
 
 
 def draw_open_close(fig):
-    # open_glottis = [0.078, 0.895]
-    # fig.set_ylim(fig.get_ylim())
-    fig.set_ylim([-0.15, 0.15])
+    fig.set_ylim(fig.get_ylim())
+    if ("ylim" in item for item in documents["energy"]):
+        ylim = next(d for i, d in enumerate(
+            documents["energy"]) if "ylim" in d)
+        fig.set_ylim(ylim["ylim"])
     plt.plot([open_glottis[0], open_glottis[0]],
              fig.get_ylim(), 'r--', linewidth=4)
     plt.plot([open_glottis[1], open_glottis[1]],
@@ -129,12 +161,24 @@ def update_xlabels(fig):
 
 
 # Plots
+fig_12a_names, fig_12a_styles, fig_12a_colors = [], [], []
+for (item, line, color) in zip(["Residual", "Pressure drive", "Rate VF work", "Rate friction work",
+                                "Rate KE", "-KE efflux", "Rate compression work",
+                                "Rate dissipation", "Stabilization",
+                                "Rate turbulent momentum transfer"],
+                               ['-', '-', '-', '--', '-',
+                                   '-.', '--', '-.', '-', '-'],
+                               ['g', 'lightgreen', 'r', 'k', 'b', 'b', 'b', 'm', 'y', 'm']):
+    if item in cv_energy.keys():
+        fig_12a_names.append(item)
+        fig_12a_styles.append(line)
+        fig_12a_colors.append(color)
+
 fig_12a = cv_energy.plot(
     x="Normalized time",
-    y=["Pressure drive", "Rate VF work", "Rate friction work", "Rate KE",
-        "-KE efflux", "Rate compression work",  "Rate dissipation", "Residual"],
-    style=['-', '-', '--', '-', '-.', '--', '-.', '-'],
-    color=['lightgreen', 'r', 'k', 'b', 'b', 'b', 'm', 'g'], markevery=20, lw=5)
+    y=fig_12a_names,
+    style=fig_12a_styles,
+    color=fig_12a_colors, markevery=20, lw=5)
 # Thicken the input
 for line in fig_12a.get_lines():
     if line.get_label() == "Pressure drive":
@@ -147,6 +191,7 @@ plt.tight_layout()
 plt.savefig(output_dir + "/12a.png", format='png')
 
 # Fig 12b: W_f, W_c, W_v
+fig_12b_names, fig_12b_styles, fig_12b_colors = [], [], []
 fig_12b = cv_energy.plot(
     x="Normalized time",
     y=["Rate friction work", "Rate compression work",  "Rate dissipation"],
